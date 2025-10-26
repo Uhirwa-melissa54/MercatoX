8a2da223cce80b56f7837d383d1adc030a970f05
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-26 21:16:58 +0200

    connected database and created entities

--- src/main/java/com/chinese/e_commerce_backend/entities/Product.java
+++ src/main/java/com/chinese/e_commerce_backend/entities/Product.java
@@ -1,10 +1,18 @@
 package com.chinese.e_commerce_backend.entities;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Product {
     @Id
+    @SequenceGenerator(
+            name = "product_id_Sequence",
+            sequenceName = "product_squence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_id_Sequence"
+    )
     private int productId;
     private String productName;
     private double productPrice;
