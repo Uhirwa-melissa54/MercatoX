fd33a7f4938682f6906af1c380061435e5653b35
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-28 13:15:57 +0200

    Fixed a bug

--- src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
+++ src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
@@ -3,6 +3,7 @@ package com.chinese.e_commerce_backend.Controllers;
 import com.chinese.e_commerce_backend.entities.User;
 import com.chinese.e_commerce_backend.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,13 @@ public class UserController {
         return userRepository.findAll();
     }
     @GetMapping("/{id}")
-    public User findById(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
+    public ResponseEntity findById(@PathVariable Long id) {
+
+        var user=userRepository.findById(id).orElse(null);
+        if(user==null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 
 }
