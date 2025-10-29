5587149e1f7c156857e362537080ac11d48f9e41
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-10-29 22:05:25 +0200

    added extraction of query paramaters

--- src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
+++ src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
@@ -3,13 +3,12 @@ package com.chinese.e_commerce_backend.Controllers;
 import com.chinese.e_commerce_backend.dto.UserDto;
 import com.chinese.e_commerce_backend.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @AllArgsConstructor
@@ -17,8 +16,13 @@ import java.util.List;
 public class UserController {
     UserRepository userRepository;
     @GetMapping
-    public List<UserDto> findAll() {
-        return userRepository.findAll()
+    public List<UserDto> findAll(
+            @RequestParam(required = false,defaultValue = "") String sort
+    ) {
+        if(Set.of("name","email").contains(sort)){
+            sort="name";
+        }
+        return userRepository.findAll(Sort.by(sort))
                 .stream()
                 .map(user-> new UserDto(user.getId(),user.getUsername(),user.getEmail())).toList();
     }
