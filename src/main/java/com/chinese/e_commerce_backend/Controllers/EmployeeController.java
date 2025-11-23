3af62207897ca6052313501677284032c683b3c4
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-23 17:42:47 +0200

    created new controlller

---
+++ src/main/java/com/chinese/e_commerce_backend/Controllers/EmployeeController.java
@@ -0,0 +1,9 @@
+package com.chinese.e_commerce_backend.Controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/employee")
+public class EmployeeController {
+    @PostMapping("/signup")
+    
+}
