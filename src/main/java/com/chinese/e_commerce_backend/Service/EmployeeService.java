23122ccb0727ec86e262bfc72fcb84b9ab4db0c1
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-23 17:33:10 +0200

    added checking email

---
+++ src/main/java/com/chinese/e_commerce_backend/Service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.chinese.e_commerce_backend.Service;
+
+import com.chinese.e_commerce_backend.repository.EmployeeRepository;
+import com.chinese.e_commerce_backend.entities.Employee;
+import java.util.List;
+
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    public boolean checkExistance(String email,String name){
+        List<Employee> exitEm=employeeRepository.checkExistance(email);
+        if(exitEm!=null){
+            return true;
+        }
+        return false;
+
+    }
+
+
+    
+}
