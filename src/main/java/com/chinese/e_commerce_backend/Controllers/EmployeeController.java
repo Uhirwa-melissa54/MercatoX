c06dd7e9d678cdefc2ba871b8a8d46109671dfa7
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-23 18:20:23 +0200

    modified Employee Controller

--- src/main/java/com/chinese/e_commerce_backend/Controllers/EmployeeController.java
+++ src/main/java/com/chinese/e_commerce_backend/Controllers/EmployeeController.java
@@ -1,9 +1,21 @@
 package com.chinese.e_commerce_backend.Controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chinese.e_commerce_backend.Service.EmployeeService;
+import com.chinese.e_commerce_backend.entities.Employee;
+import java.util.*;
+
 
 @RestController("/employee")
 public class EmployeeController {
+    private EmployeeService employeeService;
     @PostMapping("/signup")
+    ResponseEntity<RegisterResponseDto> signup(Employee employee){
+        List<Employee> exist= employeeService.checkExistance(employee.getName(),employee.getEmail());
+        employeeService.saveEmployee(employee);
+
+    }
     
 }
