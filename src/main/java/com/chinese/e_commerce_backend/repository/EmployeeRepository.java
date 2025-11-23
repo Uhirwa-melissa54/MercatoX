29708a6a70f2672277bac6767106e5d3e5f5ac5b
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-23 17:26:00 +0200

    added new methods in repoisiotory

--- src/main/java/com/chinese/e_commerce_backend/repository/EmployeeRepository.java
+++ src/main/java/com/chinese/e_commerce_backend/repository/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.chinese.e_commerce_backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.chinese.e_commerce_backend.entities.Employee;
 
 public interface EmployeeRepository  extends JpaRepository <Employee, Long>{
+    List<Employee> findByEmail(String email);
     
 }
