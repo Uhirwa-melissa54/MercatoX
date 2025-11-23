daa73068f3c6289dd0097ba20b5c0cbae67d27ec
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2025-11-23 23:48:26 +0200

    fixed a bug

--- src/main/java/com/chinese/e_commerce_backend/Service/EmployeeService.java
+++ src/main/java/com/chinese/e_commerce_backend/Service/EmployeeService.java
@@ -2,47 +2,34 @@ package com.chinese.e_commerce_backend.Service;
 
 import com.chinese.e_commerce_backend.repository.EmployeeRepository;
 import com.chinese.e_commerce_backend.entities.Employee;
+
 import java.util.List;
 
-
+import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 
+@Service
 @AllArgsConstructor
 public class EmployeeService {
-    private final EmployeeRepository employeeRepository;
-    public boolean checkExistance(String email,String name){
-        List<Employee> exitEm=employeeRepository.findByNameAndEmail(name,email);
-        if(!exitEm.isEmpty()){
-            return true;
-        }
-        return false;
 
+    private final EmployeeRepository employeeRepository;
+
+    public boolean checkExistance(String name, String email){
+        List<Employee> existEm = employeeRepository.findByNameAndEmail(name, email);
+        return !existEm.isEmpty();
     }
 
     public Employee saveEmployee(Employee employee){
         return employeeRepository.save(employee);
     }
 
-    public boolean loginEmployee(String email,String password){
-        Employee employees= employeeRepository.findByEmail(email);
-        if(employees.equals(null)){
+    public boolean loginEmployee(String email, String password){
+        Employee employee = employeeRepository.findByEmail(email);
+
+        if (employee == null) {
             return false;
         }
-        else{
-            if(employees.getPassword()==password){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        
-        }
 
+        return employee.getPassword().equals(password);
     }
-
-
-
-
-    
 }
