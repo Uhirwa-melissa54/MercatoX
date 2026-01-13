1cfb822003b05386eb29c878746880e59c087208
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-13 22:08:44 +0200

    added new method to userRepository

--- src/main/java/com/chinese/e_commerce_backend/repository/UserRepository.java
+++ src/main/java/com/chinese/e_commerce_backend/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.chinese.e_commerce_backend.repository;
 import com.chinese.e_commerce_backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository  extends JpaRepository<User,Long> {
 
+    Optional<User> findByEmailOrUsername(String email, String username);
 }
