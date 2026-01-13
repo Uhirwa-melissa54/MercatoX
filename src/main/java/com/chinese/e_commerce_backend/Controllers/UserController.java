152934a1b805a0ebb3b2b82a1e1f976f527dbcb8
Uhirwa-melissa54/ <uhirwamelissa@gmail.com>
2026-01-13 21:50:39 +0200

    created userService for signUp

--- src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
+++ src/main/java/com/chinese/e_commerce_backend/Controllers/UserController.java
@@ -15,29 +15,30 @@ import java.util.Set;
 @RequestMapping("/users")
 public class UserController {
     UserRepository userRepository;
-    @GetMapping
-    public List<UserDto> findAll(
-            @RequestParam(required = false,defaultValue = "") String sort
-    ) {
-        if(Set.of("name","email").contains(sort)){
-            sort="name";
-        }
-        return userRepository.findAll(Sort.by(sort))
-                .stream()
-                .map(user-> new UserDto(user.getId(),user.getUsername(),user.getEmail())).toList();
-    }
-    @GetMapping("/{id}")
-    public ResponseEntity findById(@PathVariable Long id) {
-
-        var user=userRepository.findById(id).orElse(null);
-        if(user==null){
-            return ResponseEntity.notFound().build();
-        }
-        var userDto=new UserDto(user.getId(),user.getUsername(),user.getEmail());
-        return ResponseEntity.ok(userDto);
-    }
+//    @GetMapping
+//    public List<UserDto> findAll(
+//            @RequestParam(required = false,defaultValue = "") String sort
+//    ) {
+//        if(Set.of("name","email").contains(sort)){
+//            sort="name";
+//        }
+//        return userRepository.findAll(Sort.by(sort))
+//                .stream()
+//                .map(user-> new UserDto(user.getId(),user.getUsername(),user.getEmail())).toList();
+//    }
+//    @GetMapping("/{id}")
+//    public ResponseEntity findById(@PathVariable Long id) {
+//
+//        var user=userRepository.findById(id).orElse(null);
+//        if(user==null){
+//            return ResponseEntity.notFound().build();
+//        }
+//        var userDto=new UserDto(user.getId(),user.getUsername(),user.getEmail());
+//        return ResponseEntity.ok(userDto);
+//    }
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody UserDto userDto){
+
 
     }
 
--- src/main/java/com/chinese/e_commerce_backend/Service/UserService.java
+++ src/main/java/com/chinese/e_commerce_backend/Service/UserService.java
@@ -1,5 +1,22 @@
 package com.chinese.e_commerce_backend.Service;
 
+import com.chinese.e_commerce_backend.dto.UserDto;
+import com.chinese.e_commerce_backend.entities.User;
+import com.chinese.e_commerce_backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserService {
+    UserRepository userRepository;
+    public User register(User user){
+
+User saved=userRepository.save(user);
+        return saved;
+
+    }
     
 }
